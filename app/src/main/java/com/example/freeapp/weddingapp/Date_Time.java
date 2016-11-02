eeb3d8309ef188166784a413bdd2b5a7d9cba6ac
parvesh parmar <parvesh.parmar@mail.vinove.com>
2016-11-02 18:51:17 +0530

    first commit

---
+++ app/src/main/java/com/example/freeapp/weddingapp/Date_Time.java
@@ -0,0 +1,18 @@
+package com.example.freeapp.weddingapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vinove on 2/11/16.
+ */
+public class Date_Time extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.date_time);
+    }
+}
---
+++ app/src/main/java/com/example/freeapp/weddingapp/Invitation.java
@@ -0,0 +1,17 @@
+package com.example.freeapp.weddingapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vinove on 2/11/16.
+ */
+public class Invitation extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.invitation);
+    }
+}
